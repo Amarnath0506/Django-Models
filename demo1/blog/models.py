3e86dd643882681ab54a1a413e50820f4869e728
Amarnath0506 <baduguamarnath12@gmail.com>
2021-07-07 19:50:31 +0530

    changes in membership models

--- demo1/blog/models.py
+++ demo1/blog/models.py
@@ -42,3 +42,6 @@ class Membership(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     date_joined = models.DateField()
     invite_reason = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.invite_reason
